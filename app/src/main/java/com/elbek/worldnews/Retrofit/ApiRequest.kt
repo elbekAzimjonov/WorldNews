7a5375198b5f6995006e058f56b7723b248d2081
Elbek Azimjonov <75204275+elbekAzimjonov@users.noreply.github.com>
2022-01-31 10:13:44 +0500

    Update ApiRequest.kt

--- app/src/main/java/com/elbek/worldnews/Retrofit/ApiRequest.kt
+++ app/src/main/java/com/elbek/worldnews/Retrofit/ApiRequest.kt
@@ -4,7 +4,7 @@ import com.elbek.worldnews.api.NewsApiJson
 import retrofit2.http.GET
 
 interface ApiRequest {
-   @GET("/v2/everything?q=tesla&from=2021-12-15&sortBy=publishedAt&apiKey=491349b3a828420ba9dda00843614f8e")
+   @GET("/v2/everything?q=tesla&from=2021-12-15&sortBy=publishedAt&apiKey=YouApiKey")
    suspend fun getNewsApi():NewsApiJson
 
 }
